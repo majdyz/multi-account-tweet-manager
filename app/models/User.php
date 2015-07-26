6faf99dafabb85e809b607507a4fd7f418ae551c
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2015-07-26 16:40:18 +0700

    fix user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,3 +1,13 @@
 <?php
 
-class User extends Model {}
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model {
+
+    protected $table = 'user';
+    public $timestamps = false;
+
+    public function twitterAccounts() {
+        return $this->belongsToMany('TwitterAccount', 'UserTwitterAccount', 'user_id', 'twitter_id');
+    }
+}
