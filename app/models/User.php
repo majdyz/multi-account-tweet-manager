c011620728623f2bac263588396b4fe22f12c960
Zamil Majdy <z.majdy@yahoo.com>
2015-07-27 21:10:31 +0700

    add user checking on post tweet

--- app/models/User.php
+++ app/models/User.php
@@ -10,4 +10,17 @@ class User extends Model {
     public function twitterAccounts() {
         return $this->belongsToMany('TwitterAccount', 'twitteraccount_user', 'user_id', 'twitter_id');
     }
+
+    public static function getActiveAccounts() {
+    	return User::find(Sentry::getUser()->id)->twitterAccounts()->where('status', '>', 1)->get();
+    }
+
+    public function hasThisAccount($id) {
+    	if ($this->twitterAccounts()->where('id',$id)->count() > 0) {
+    		return true;
+    	}
+    	else {
+    		return false;
+    	}
+    }
 }
