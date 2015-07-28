6988c74a0fdc17cb6f39f3f9eb893ad792823ef8
Zamil Majdy <z.majdy@yahoo.com>
2015-07-28 23:16:50 +0700

    fix js for check box + initialize form on edit mode

--- public/assets/js/app/tweet.js
+++ public/assets/js/app/tweet.js
@@ -38,6 +38,7 @@ $(function(){
 
         $.get(global.baseUrl+'admin/tweet/'+global.tweetset_id+'/'+$tweetid, function(resp){
             if(resp.success){
+                
                 $('#tweet-form-data').each(function(){
                     this.reset();
                 });
@@ -49,7 +50,11 @@ $(function(){
 
                 for(var a in $tweet){
                     $('#tweet_'+a).val($tweet[a]);
-                    console.log(a+","+$tweet[a]);
+                }
+
+                for (var a in $tweet['medias']) {
+                    console.log ($tweet['medias'][a]['id']);
+                    $('#tweet_media_'+$tweet['medias'][a]['id']).attr('checked',true);
                 }
 
                 $('#btn-tweet-save').attr('data-method', 'PUT');
@@ -104,7 +109,7 @@ $(function(){
         e.preventDefault();
 
         var $button = $(this),
-            $tweetdata = $('#tweet-form-data').serialize(),
+            $tweetdata = $('#tweet-form-data').serializeArray(),
             $method = $(this).attr('data-method'),
             $url = ($method == 'POST') ? global.baseUrl+'admin/tweet/'+global.tweetset_id : global.baseUrl+'admin/tweet/'+global.tweetset_id+'/'+$('#tweet_id').val();
 
@@ -112,6 +117,14 @@ $(function(){
         $button.html('saving...');
         $loader.show();
 
+        $selected = [];
+        $('#tweet_medias input:checked').each(function() {
+            $selected.push($(this).attr('value'));
+        });
+
+        $tweetdata.push({'name' : 'medias' , 'value' : JSON.stringify($selected)});
+
+        // console.log($tweetdata);
 
         $.ajax({
             url: $url,
@@ -131,14 +144,12 @@ $(function(){
                         /** append tweet to new row */
                         $('#tweet-table').append(
                             '<tr id="tweet-row-'+resp.data.id+'">'+
-                                '<td>'+tweet.id+'</td>'+
                                 '<td>'+tweet.name+'</td>'+
                                 '<td>'+tweet.text+'</td>'+
                                 '<td>'+tweet.mentions+'</td>'+
                                 '<td>'+tweet.hashtags+'</td>'+
                                 '<td>'+tweet.media+'</td>'+
                                 '<td>'+tweet.updated_at+'</td>'+
-                                '<td>'+tweet.created_at+'</td>'+
                                 '<td class="text-center">'+
                                     '<a data-id="'+tweet.id+'" class="btn btn-xs btn-primary btn-tweet-edit" href="#"><i class="fa fa-edit fa-fw"></i>Edit</a>'+
                                     '<a data-id="'+tweet.id+'" class="btn btn-xs btn-danger btn-tweet-delete" href="#" style="margin-left: 5px"><i class="fa fa-times fa-fw"></i>Remove</a>'+
@@ -146,18 +157,20 @@ $(function(){
                             '</tr>'
                         );
                     }else{
+                        console.log(resp.data);
 
                         var $fields = $('#tweet-row-'+resp.data.id+' td');
                         if (tweet.tweetset_id != global.tweetset_id) {
                             $fields.hide();
                         }
-                        $($fields[1]).html(tweet.name);
-                        $($fields[2]).html(tweet.text);
-                        $($fields[3]).html(tweet.mentions);
-                        $($fields[4]).html(tweet.hashtags);
-                        $($fields[5]).html(tweet.media);
-                        $($fields[6]).html(tweet.updated_at);
-                        $($fields[7]).html(tweet.created_at);
+
+
+                        $($fields[0]).html(tweet.name);
+                        $($fields[1]).html(tweet.text);
+                        $($fields[2]).html(tweet.mentions);
+                        $($fields[3]).html(tweet.hashtags);
+                        $($fields[4]).html(tweet.media);
+                        $($fields[5]).html(tweet.updated_at);
                     }
 
                     /** reset the form and hide modal form */
