eacf7a9ee0d5b14b35af969bc17fb4495ae68730
Muhammad Rizki A.R.M <rizki.idea@gmail.com>
2015-07-30 09:25:51 +0700

    Remove unused upload method on Media model

--- app/models/Media.php
+++ app/models/Media.php
@@ -20,22 +20,6 @@ class Media extends Model
         return $this->belongsToMany('Tweet', 'tweet_media', 'media_id', 'tweet_id');
     }
 
-    public static function upload($base64)
-    {
-        $connection = new TwitterOAuth(
-            TwitterAccount::getCredentialsTwitter()['consumer_key'],
-            TwitterAccount::getCredentialsTwitter()['consumer_secret'],
-            TwitterAccount::getCredentialsTwitter()['oauth_token'],
-            TwitterAccount::getCredentialsTwitter()['oauth_token_secret']);
-        $media1 = $connection->upload('media/upload', array('media' => $base64));
-        // $parameters = array(
-        //     'status' => '',
-        //     'media_ids' => implode(',', array($media1->media_id_string)),
-        // );
-        // $result = $connection->post('statuses/update', $parameters);
-        return $media1;
-    }
-
     public function isUserHas()
     {
         if ($this->user_id == \Sentry::getUser()->id) {
