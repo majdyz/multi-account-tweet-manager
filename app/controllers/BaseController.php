9b16ab798e38688e459b74e69c45ba2f9d1e0ad6
Zamil Majdy <z.majdy@yahoo.com>
2015-10-15 00:06:11 +0700

    fix checkbox error

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -246,8 +246,9 @@ class BaseController
         $this->loadJs("bootstrap.min.js");
         $this->loadJs("plugins/metisMenu/jquery.metisMenu.js");
         $this->loadJs("sb-admin.js");
-        $this->loadJs("material.min.js");
         $this->loadJs("ripples.min.js");
+        $this->loadJs("material.min.js");
+        $this->loadJs("init-material.js");
     }
 
     /**
